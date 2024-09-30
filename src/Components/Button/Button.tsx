32cab7ef82f201cc30103c93a2ecc6ba63351c1c
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-30 09:38:44 -0600

    feat: add button component

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import './Button.css'
+
+export default function Button({
+  children,
+  className,
+  type = 'button',
+  onClick,
+}:{
+  children?: React.ReactNode;
+  className?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <>
+      <button 
+        className={`button${!!className ? ' ' + className : ''}`}
+        type={type}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </>
+  )
+}
