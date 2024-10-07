2a9314ad961cf148d50834b410e68e238a36a050
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-10-06 21:01:55 -0600

    Feat: add modClass to button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,18 +3,23 @@ import './Button.css'
 export default function Button({
   children,
   className,
+  modClass = '',
   type = 'button',
   onClick = () => {},
 }:{
   children?: React.ReactNode;
   className?: string;
+  modClass?: string;
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }) {
+  const mod = modClass ? ` button_${modClass}` : '';
+  const newClass = !!className ? ' ' + className : '';
+
   return (
     <>
       <button 
-        className={`button${!!className ? ' ' + className : ''}`}
+        className={`button${mod}${newClass}`}
         type={type}
         onClick={onClick}
       >
