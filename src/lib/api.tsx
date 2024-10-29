c09d44392784ce4b8f99e7e97dcc7c340b48817c
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-10-29 12:31:20 -0600

    Refactor: change the names in api's file

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -3,57 +3,57 @@ import { Countries, ExtendedCountry } from "./types"
 
 const BASE_URL = 'https://restcountries.com/v3.1/'
 const fields = '?fields=name'
-const fieldsOfCards = fields + ',flags,population,region,capital'
-const extendedFields = fieldsOfCards + ',currencies,subregion,tld,currencies,languages,borders&fullText=true'
+const shortFields = fields + ',flags,population,region,capital'
+const extendedFields = shortFields + ',currencies,subregion,tld,currencies,languages,borders&fullText=true'
 
 function confirm(res: Response) {
   if (res.ok) return res.json()
   return Promise.reject(`Error: ${res.status}`)
 }
 
-function getOne(params: string) {
+function getDetailedCountry(params: string): Promise<ExtendedCountry> {
   return fetch(BASE_URL + params + extendedFields)
     .then(confirm)
     .then(res => res[0])
 }
 
-function get(params: string) {
-  return fetch(BASE_URL + params + fieldsOfCards)
+function getSummarizedCountries(params: string): Promise<Countries> {
+  return fetch(BASE_URL + params + shortFields)
     .then(confirm)
 }
 
-function getByAlpha(code: string) {
+function getCountryNameByAlpha(code: string) {
   return fetch(BASE_URL + `alpha${fields}&codes=${code}` )
     .then(confirm)
 }
 
-const firstCountries = ['germany', 'united states of america', 'brazil', 'iceland', 'afghanistan', 'Åland Islands', 'albania', 'algeria']
-  .map((country) => get(`name/${country}`))
+const defaultCountries = ['germany', 'united states of america', 'brazil', 'iceland', 'afghanistan', 'Åland Islands', 'albania', 'algeria']
+  .map((country) => getSummarizedCountries(`name/${country}`))
 
 export async function getCountries({request}: {request: Request}): Promise<Countries> {
   const { searchParams: search } = new URL(request.url)
 
-  if(!search.has('country')) return await Promise.all(firstCountries).then((res) => res.flat())
+  if(!search.has('country')) return await Promise.all(defaultCountries).then((res) => res.flat())
 
   const country = search.get('country')
 
   if(search.has('region')) {
-    const region: Countries = await get(`region/${search.get('region')}`)
+    const region = await getSummarizedCountries(`region/${search.get('region')}`)
 
     if(country) return region.filter(({name: {common}}) => common.includes(country))
 
     return region
   }
 
-  if(!country) return await get('all')
+  if(!country) return await getSummarizedCountries('all')
 
-  return await get(`name/${country}`)
+  return await getSummarizedCountries(`name/${country}`)
 }
 
 export async function getTheCountry({ params }: { params: Params<'name'> }) {
-  const country: ExtendedCountry = await getOne(`name/${params.name!}`)
+  const country = await getDetailedCountry(`name/${params.name!}`)
   const { borders: bor } = country
-  const borders = bor.length ? await getByAlpha(bor.join(',')) : []
+  const borders = bor.length ? await getCountryNameByAlpha(bor.join(',')) : []
 
   return { country, borders }
 }
